d13b18f55e2028dbca188760da05b1a4582c8682
gr33nh00d <sawyerpayne1@gmail.com>
2016-03-31 21:04:59 -0600

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -5,7 +5,7 @@ cityID=0
 for i in {1..2}
 do
   # go run callOpenmap.go $i > output.txt
-  cityID= sed -n "$ip" cityIDs.txt
+  cityID="$(sed -n $i'p' cityIDs.txt)"
   echo $cityID
   # go run /home/topology_weather/callOpenmap.go $cityID > output.txt
   # go run parse.go >> data.txt
