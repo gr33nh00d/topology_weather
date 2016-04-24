c4d0cb12cec6326bf04dcc4797c919e000ca93cd
gr33nh00d <sawyerpayne1@gmail.com>
2016-04-24 14:14:34 -0600

    updating collect script

--- collect.sh
+++ collect.sh
@@ -5,10 +5,14 @@ F="$1"
 P="$2"
 echo "$F"
 echo "$P"
-for i in {1..144}
-do
-  cityID="$(sed -n $i'p' cityIDs.txt)"
-  go run $F $cityID > output.txt
-  go run $P >> data.txt
-  echo $i
-done
+  while [[ true ]]; do
+    rm output.txt
+    touch output.txt
+    for i in {1..144}
+    do
+      cityID="$(sed -n $i'p' cityIDs.txt)"
+      go run $F $cityID >> output.txt
+    done
+    go run $P
+    sleep 21600
+  done
